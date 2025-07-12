461250f1d7eef8da32d70671748182c07fbb4315
Saad Shaikh <saadshaikh400695@gmail.com>
2025-07-12 13:12:35 +0530

    Linked list 04 Input of nodes and values

---
+++ Semister 3/DS/Practice/linked_list04.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node {
+    int data;
+    struct node *link;
+};
+
+int main(){
+    struct node *head = NULL, *current = NULL;
+    int n, value;
+
+    printf("Enter the number of nodes: ");
+    scanf("%d", &n);
+
+    for (int i = 0; i < n; i++){
+        printf("Enter value for node %d: ", i + 1);
+        scanf("%d", &value);
+
+        struct node *Nodes = (struct node*)malloc(sizeof(struct node));
+        Nodes -> data = value;
+        Nodes -> link = NULL;
+
+        if (head == NULL){
+            head = Nodes;
+            current = head;
+        } else {
+            current -> link = Nodes;
+            current = Nodes;
+        }
+    }
+
+    current = head;
+    while (current != NULL){
+        printf("%d\n", current -> data);
+        current = current -> link;
+    }
+
+    return 0;
+}
