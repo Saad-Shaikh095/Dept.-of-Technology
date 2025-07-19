a4647b8d2a9d9db8dc07fc5cae0016af6bf1a7d4
Saad Shaikh <saadshaikh400695@gmail.com>
2025-07-19 20:15:30 +0530

    linked list 01 CLL DS

---
+++ Semister 3/DS/Practice/Linked List/CLL/linked_list01.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node {
+    int data;
+    struct node *next;
+};
+
+int main(){
+    struct node *head = NULL;
+    struct node *one = NULL;
+    struct node *two = NULL;
+    struct node *three = NULL;
+
+    head = malloc(sizeof(struct node));
+    one = malloc(sizeof(struct node));
+    two = malloc(sizeof(struct node));
+    three = malloc(sizeof(struct node));
+
+    head -> data= 100;
+    one -> data = 200;
+    two -> data = 300;
+    three -> data = 400;
+
+    head -> next = one;
+    one -> next = two;
+    two -> next = three;
+    three -> next = head;
+
+    struct node *current = head;
+
+    do {
+        printf("%d\n", current -> data);
+        current = current -> next;
+    }
+    while (current != head);
+    printf("\n");
+
+    return 0;
+}
